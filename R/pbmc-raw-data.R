aec88e5d515feea3ed130979b380a4308296814c
lvlahos343 <lvlahos343@gmail.com>
2022-06-15 15:04:00 -0400

    added pbmc.raw data

---
+++ R/pbmc-raw-data.R
@@ -0,0 +1,18 @@
+#' Single-Cell RNASeq from PBMC data
+#' 
+#' Human peripheral blood mononuclear cells sequenced using the 10x Chromium platform.
+#' This dataset has been subset to 1,000 samples and contains 58,828 genes. Genes are in rows
+#' and samples are in columns. Genes are labeled with HGNC names.
+#' 
+#' @docType data
+#' 
+#' @usage data(pbmc.raw)
+#' 
+#' @format A matrix.
+#' 
+#' @source "[insert citation]"
+#' 
+#' @examples 
+#' data(pbmc.raw)
+#' \donttest{dim(pbmc.raw)}
+"pbmc.raw"
